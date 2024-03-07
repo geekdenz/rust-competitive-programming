6f8b5eae2766cae98bca18bb104703bf4cc6aaec
Tim-Hinnerk Heuer <tim.heuer@triotech.co.nz>
2024-03-07 17:51:30 +1300

    make code work as Solution

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::collections::HashMap;
 
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut m = HashMap::new();
-    for (i, &num) in nums.iter().enumerate() {
-        if let Some(&j) = m.get(&(target - num)) {
-            return vec![j as i32, i as i32];
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut m = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            if let Some(&j) = m.get(&(target - num)) {
+                return vec![j as i32, i as i32];
+            }
+            m.insert(num, i);
         }
-        m.insert(num, i);
+        return vec![];
     }
-    return vec![];
 }
 fn main() {
     let nums = vec![2, 7, 11, 15];
     let target = 9;
-    let result = two_sum(nums, target);
+    let result = Solution::two_sum(nums, target);
     println!("{:?}", result);
 }
