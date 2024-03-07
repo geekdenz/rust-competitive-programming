418b7b73f78357728dcb4fa73302b4c754b10a27
Tim-Hinnerk Heuer <tim.heuer@triotech.co.nz>
2024-03-07 16:06:00 +1300

    add project

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut m = HashMap::new();
+    for (i, &num) in nums.iter().enumerate() {
+        if let Some(&j) = m.get(&(target - num)) {
+            return vec![j as i32, i as i32];
+        }
+        m.insert(num, i);
+    }
+    return vec![];
+}
+fn main() {
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+    let result = two_sum(nums, target);
+    println!("{:?}", result);
+}
